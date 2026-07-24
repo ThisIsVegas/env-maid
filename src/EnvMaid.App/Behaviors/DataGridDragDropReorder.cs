865fe8df2df3abde4ed7237c9850df21a4dd5b77
Veerakran Sereerungruangkul <thisisvegas007@gmail.com>
2026-07-24 16:49:15 +0700

    refactor(ui): Refactor ListViewDragDropReorder to DataGridDragDropReorder

--- src/EnvMaid.App/Behaviors/DataGridDragDropReorder.cs
+++ src/EnvMaid.App/Behaviors/DataGridDragDropReorder.cs
@@ -6,11 +6,11 @@ using System.Windows.Media;
 
 namespace EnvMaid.App.Behaviors;
 
-public static class ListViewDragDropReorder
+public static class DataGridDragDropReorder
 {
     public static readonly DependencyProperty IsEnabledProperty =
         DependencyProperty.RegisterAttached(
-            "IsEnabled", typeof(bool), typeof(ListViewDragDropReorder),
+            "IsEnabled", typeof(bool), typeof(DataGridDragDropReorder),
             new PropertyMetadata(false, OnIsEnabledChanged));
 
     public static void SetIsEnabled(DependencyObject element, bool value) => element.SetValue(IsEnabledProperty, value);
@@ -21,12 +21,12 @@ public static class ListViewDragDropReorder
 
     private static void OnIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
-        if (d is not ListView listView || e.NewValue is not true) return;
+        if (d is not DataGrid grid || e.NewValue is not true) return;
 
-        listView.AllowDrop = true;
-        listView.PreviewMouseLeftButtonDown += (_, args) => _dragStart = args.GetPosition(null);
+        grid.AllowDrop = true;
+        grid.PreviewMouseLeftButtonDown += (_, args) => _dragStart = args.GetPosition(null);
 
-        listView.PreviewMouseMove += (_, args) =>
+        grid.PreviewMouseMove += (_, args) =>
         {
             if (args.LeftButton != MouseButtonState.Pressed) return;
 
@@ -34,44 +34,37 @@ public static class ListViewDragDropReorder
             var diff = _dragStart - current;
             if (Math.Abs(diff.X) < 5 && Math.Abs(diff.Y) < 5) return;
 
-            var item = listView.SelectedItem;
+            var item = grid.SelectedItem;
             if (item is null) return;
 
             _draggedItem = item;
-            DragDrop.DoDragDrop(listView, item, DragDropEffects.Move);
+            DragDrop.DoDragDrop(grid, item, DragDropEffects.Move);
         };
 
-        listView.Drop += (_, args) =>
+        grid.Drop += (_, args) =>
         {
             if (_draggedItem is null) return;
-            if (listView.ItemsSource is not IList items) return;
+            if (grid.ItemsSource is not IList items) return;
 
-            var targetItem = FindListViewItem(args.OriginalSource as DependencyObject);
-            if (targetItem?.DataContext is null) return;
+            var targetRow = FindDataGridRow(args.OriginalSource as DependencyObject);
+            if (targetRow?.Item is null) return;
 
             var oldIndex = items.IndexOf(_draggedItem);
-            var newIndex = items.IndexOf(targetItem.DataContext);
+            var newIndex = items.IndexOf(targetRow.Item);
             if (oldIndex < 0 || newIndex < 0 || oldIndex == newIndex) return;
 
-            if (items is System.Collections.ObjectModel.ObservableCollection<object> generic)
-            {
-                generic.Move(oldIndex, newIndex);
-            }
-            else
-            {
-                var moveMethod = items.GetType().GetMethod("Move");
-                moveMethod?.Invoke(items, new object[] { oldIndex, newIndex });
-            }
+            var moveMethod = items.GetType().GetMethod("Move");
+            moveMethod?.Invoke(items, new object[] { oldIndex, newIndex });
 
             _draggedItem = null;
         };
     }
 
-    private static ListViewItem? FindListViewItem(DependencyObject? source)
+    private static DataGridRow? FindDataGridRow(DependencyObject? source)
     {
-        while (source is not null && source is not ListViewItem)
+        while (source is not null && source is not DataGridRow)
             source = VisualTreeHelper.GetParent(source);
 
-        return source as ListViewItem;
+        return source as DataGridRow;
     }
 }
